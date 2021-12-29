e3c3d5e913fa42ed12d07286b33fd7d9ac84dacd
Brett Shenk <bshenk@goldenpeakmedia.com>
2021-12-28 19:02:38 -0500

    See changelog  1.1.2 | 12/28/2021 | Build

--- specific_site.php
+++ specific_site.php
@@ -9,7 +9,7 @@
 /**
  * @uses If it's not defined, by default it will return `production`.
  * @uses Other possible options are:  [production, staging, development, local]
- **/
+**/
 define( 'WP_ENVIRONMENT_TYPE', 'development' );
 
 
@@ -21,3 +21,65 @@ if(wp_get_environment_type() === 'staging') {
 } else {
     // Do something else
 }
+
+
+
+
+/**********************************************************************************/
+/**
+ * @package Other Useful Functions
+ * @todo    A collection of other useful functions for finding what site you are on.
+**/
+
+
+/**
+ * Theme URL
+ * @uses Return your current theme directory.
+ * @uses If using a child theme, will return that.
+**/
+define( 'THEME_DIR',  get_stylesheet_directory_uri() );
+
+// Example
+echo THEME_DIR;
+// Will output: https://domain.com/wp-content/themes/your-theme-child
+
+
+
+
+/**
+ * Theme Parent URL
+ * @uses Return your current theme directory.
+ * @uses If using a child theme, this will return your parent theme.
+**/
+define( 'PARENT_THEME',  get_template_directory() );
+
+// Example
+echo PARENT_THEME;
+// Will output: https://domain.com/wp-content/themes/your-theme
+
+
+
+
+/**
+ * Absolute Theme URL
+ * @uses Return your current directory.
+ * @uses Some functions don't like relative paths.
+**/
+define( 'THEME_ABS',  dirname( __FILE__ ) );
+
+// Example
+echo THEME_ABS;
+// Will output: /dom12345/wp-content/themes/your-theme-child
+
+
+
+
+/**
+ * Site URL
+ * @uses Returning the domain of the site in URL format.
+ * @uses Note the lack of trailing slash. Adding it inside home_url('/') will add it.
+**/
+
+// Example
+echo esc_url( home_url() );
+// Will output: https://domain.com
